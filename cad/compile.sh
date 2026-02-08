b7e5ac02eded592ecb80aa2fd9bc9dc6a0562fed
Joshua <joshdlucas@gmail.com>
2026-02-07 22:39:34 -0600

    Add compile script.

---
+++ cad/compile.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+
+cat \
+ components/battery.scad \
+ components/btu.scad \
+ components/center_block.scad \
+ components/frame.scad \
+ components/hinge.scad \
+ components/key_placement.scad \
+ components/keys.scad \
+ components/magnetic_connector.scad \
+ components/mcu.scad \
+ components/plates.scad \
+ components/trackball_sensor.scad \
+ components/trackball.scad \
+ keycaps/keycaps_nolib_compiled.scad \
+ library/animation.scad \
+ library/choc_switch.scad \
+ library/fillet.scad \
+ library/gateron_ks33.scad \
+ library/globals.scad \
+ library/math.scad \
+ library/path.scad \
+ library/piano_hinge.scad \
+ library/rainbow.scad \
+ library/screw_globals.scad \
+ library/screw.scad \
+ library/shapes.scad \
+ library/test.scad \
+ library/utility.scad \
+ androphage.scad \
+ assembly.scad \
+ color.scad \
+ derives.scad \
+ globals.scad \
+ objects.scad \
+| grep -v '^include' > ./androphage_compiled.scad
+echo "androphage_compiled.scad written."
