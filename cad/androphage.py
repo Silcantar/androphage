0975696ee7a6759bb7fd7118f0a31e5b5c17c5ee
Joshua Lucas <j.lucas@prime-controls.com>
2026-02-27 16:56:37 -0600

    Work on key placement.

--- cad/androphage.py
+++ cad/androphage.py
@@ -1,6 +1,21 @@
+import typing
+import yaml
+from os import PathLike
+
 import build123d as bd
 
 from androphage_common import *
 
+class Androphage(bd.BasePartObject):
+    def __init__(self, yamlpath: PathLike):
+        with open(yamlpath) as yamlfile:
+            self.params = yaml.safe_load(yamlfile)
+        build()
+
+    def build(self):
+        pass
+
+
 if __name__ == '__main__':
     from ocp_vscode import show
+    show(Androphage('androphage.yaml'))
--- cad/key_placement.py
+++ cad/key_placement.py
@@ -1,5 +1,93 @@
 import typing
-from collections.abc import Iterable
+from collections.abc import Sequence
+from math import sin, cos
+
+import build123d as bd
 
 from androphage_common import *
 
+def key_positions(defs: dict[str, any]) -> dict[str, list[bd.Vector]]:
+    spacing = defs['spacing']
+    cutout = defs['cutout']
+
+    return place_columns(defs['columns'])
+
+def place_columns(columns: dict[str, any]) -> dict[str, list[bd.Vector]]:
+    column = columns.popitem()
+
+    if len(columns) == 0:
+        return key_positions
+    else:
+        return key_positions.append(place_columns(columns))
+
+
+
+testyaml = '''
+  spacing: [18, 17]
+  cutout: [14, 14]
+  columns:
+    thumb_inner:
+      suppress: false
+      keys: 1
+      stagger: 0
+      splay: 0
+      spread: 0
+    thumb_home:
+      suppress: false
+      keys: 2
+      stagger: 0
+      splay: 10
+      spread: 0
+    thumb_outer1:
+      suppress: false
+      keys: 1
+      stagger: 0
+      splay: 10
+      spread: 0
+    thumb_outer2:
+      suppress: false
+      keys: 1
+      stagger: 0
+      splay: 10
+      spread: 0
+    finger_inner:
+      suppress: false
+      keys: 3
+      stagger: 1
+      splay: 0
+      spread: -1
+    finger_index:
+      suppress: false
+      keys: 3
+      stagger: 0
+      splay: 0
+      spread: 1
+    finger_middle:
+      suppress: false
+      keys: 4
+      stagger: -0.5
+      splay: 0
+      spread: 1
+    finger_ring:
+      suppress: false
+      keys: 4
+      stagger: -0.5
+      splay: 5
+      spread: 1
+    finger_pinky:
+      suppress: false
+      keys: 3
+      stagger: 0.5
+      splay: 10
+      spread: 1
+    finger_outer:
+      suppress: true
+      keys: 2
+      stagger: 0.5
+      splay: 0
+      spread: 1
+'''
+
+if __name__ == '__main__':
+    import yaml
+    print(key_positions(yaml.safe_load(testyaml)))
