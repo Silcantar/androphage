53452cd5c5e5c654d03080a45b7ce715428be068
Joshua Lucas <j.lucas@prime-controls.com>
2026-03-03 16:51:17 -0600

    Work on key locations.

--- cad/key_locations.py
+++ cad/key_locations.py
@@ -6,21 +6,30 @@ import build123d as bd
 
 from androphage_common import *
 
-def key_positions(defs: dict[str, any]) -> dict[str, list[bd.Vector]]:
-    spacing = defs['spacing']
-    cutout = defs['cutout']
+def key_locations(defs: dict[str, any]) -> bd.LocationList:
+    spacing = bd.Vector(defs['spacing'])
+    cutout = bd.Vector(defs['cutout'])
 
-    return place_columns(defs['columns'])
-
-def place_columns(columns: dict[str, any]) -> dict[str, list[bd.Vector]]:
-    column = columns.popitem()
-
-    if len(columns) == 0:
-        return key_positions
-    else:
-        return key_positions.append(place_columns(columns))
-
-
+    positions: list[bd.Location] = []
+    origin = bd.Location([0, 0])
+    columns = defs['columns']
+    for column_key in columns:
+        column = columns[column_key]
+        if column['suppress']:
+            next
+        for i in range(0, column['keys']):
+            splay = -column['splay']
+            positions.append(
+                origin * bd.Location(
+                    position=[0, i*spacing.Y],
+                    orientation=[0, 0, -column['splay']]
+                )
+            )
+        origin *= bd.Location(
+            position=[column['spread']*spacing.X, column['stagger']*spacing.Y],
+            orientation=[0, 0, -column['splay']]
+        )
+    return bd.LocationList(positions)
 
 testyaml = '''
   spacing: [18, 17]
@@ -31,25 +40,25 @@ testyaml = '''
       keys: 1
       stagger: 0
       splay: 0
-      spread: 0
+      spread: 1
     thumb_home:
       suppress: false
       keys: 2
       stagger: 0
       splay: 10
-      spread: 0
+      spread: 1
     thumb_outer1:
       suppress: false
       keys: 1
       stagger: 0
       splay: 10
-      spread: 0
+      spread: 1
     thumb_outer2:
       suppress: false
       keys: 1
       stagger: 0
       splay: 10
-      spread: 0
+      spread: 1
     finger_inner:
       suppress: false
       keys: 3
@@ -90,4 +99,11 @@ testyaml = '''
 
 if __name__ == '__main__':
     import yaml
-    print(key_positions(yaml.safe_load(testyaml)))
+    from ocp_vscode import show
+    kl = key_locations(yaml.safe_load(testyaml))
+    print(kl.locations)
+    with bd.BuildPart() as keys:
+        bd.Box(1,1,1)
+        with kl:
+            bd.Box(1, 1, 1)
+    show(keys)
