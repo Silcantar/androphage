2372919aae1867dd1827b8a938c975ed9e450601
Joshua Lucas <joshdlucas@gmail.com>
2026-03-10 23:16:13 -0500

    Start writing code to generate plate outline.

--- cad/androphage_common.py
+++ cad/androphage_common.py
@@ -1,10 +1,17 @@
 import typing
 from collections.abc import Iterable, Sequence
 from dataclasses import dataclass
+from math import cos, sin, radians
 
 import build123d as bd
 
 EPS = 0.001
+
+def cosd(angle: float) -> float:
+    return cos(radians(angle))
+
+def sind(angle: float) -> float:
+    return sin(radians(angle))
 
 AlignLike = bd.Align | tuple[bd.Align, bd.Align, bd.Align]
 
@@ -85,7 +92,7 @@ class Column:
                 self.__dict__[key] = self._defaults[key]
 
 
-class LocationDict(dict[str, bd.Location]):
+class LocationDict(dict):
     def __init__(self, **kwargs):
         super().__init__(kwargs)
 
@@ -96,7 +103,7 @@ class LocationDict(dict[str, bd.Location]):
 def key_locations(
     column_defs: dict[str, any],
     spacing: Sequence[float, float]
-) -> bd.LocationList:
+) -> LocationDict:
     spc = bd.Vector(spacing)
     locations = LocationDict()
     origin = bd.Location((0, 0))
@@ -120,8 +127,58 @@ def key_locations(
     return locations
 
 
-def plate_outline() -> bd.Sketch:
+def plate_outline(
+    key_locations: LocationDict,
+    spacing: Sequence[float, float],
+    hinge_length: float
+) -> bd.Face:
+    spc = bd.Vector(spacing)
+    bottom = 0
+    top = spc.Y
+    left = 0
+    right = spc.X
+    center = spc.X/2
+    middle = spc.Y/2
+    hinge_back_point = (
+        key_locations['thumb_inner_0']
+        * bd.Pos(left, top)
+        * bd.Rot(Z=-45)
+        * bd.Pos(0, hinge_length)
+    )
     with bd.BuildSketch() as sketch:
         with bd.BuildLine() as outline:
-            pass
-    return sketch
+            bd.Polyline(
+                (key_locations['thumb_outer2_0'] * bd.Pos(center, bottom)).position,
+                (key_locations['finger_ring_0'] * bd.Pos(right, bottom)).position,
+                (key_locations['finger_pinky_0'] * bd.Pos(right, bottom)).position,
+                (key_locations['finger_pinky_2'] * bd.Pos(right, top)).position,
+            )
+            bd.ThreePointArc(
+                (key_locations['finger_pinky_2'] * bd.Pos(right, top)).position,
+                (key_locations['finger_ring_3'] * bd.Pos(right, top)).position,
+                (key_locations['finger_middle_3'] * bd.Pos(right, top)).position,
+            )
+            bd.Line(
+                (key_locations['finger_middle_3'] * bd.Pos(right, top)).position,
+                (key_locations['finger_middle_3'] * bd.Pos(left, top)).position,
+            )
+                #(key_locations['finger_inner_2'] * bd.Pos(left, top)).position,
+            bd.TangentArc(
+                hinge_back_point.position,
+                (key_locations['finger_middle_3'] * bd.Pos(left, top)).position,
+                tangent=(bd.Rot(hinge_back_point.orientation) * bd.Pos(1,0)).position
+            )
+            bd.Polyline(
+                hinge_back_point.position,
+                (key_locations['thumb_inner_0'] * bd.Pos(left, top)).position,
+                (key_locations['thumb_inner_0'] * bd.Pos(left, bottom)).position,
+                (key_locations['thumb_inner_0'] * bd.Pos(center, bottom)).position
+            )
+            bd.ThreePointArc(
+                (key_locations['thumb_inner_0'] * bd.Pos(center, bottom)).position,
+                (key_locations['thumb_home_0'] * bd.Pos(right, bottom)).position,
+                (key_locations['thumb_outer2_0'] * bd.Pos(center, bottom)).position
+            )
+        bd.make_face()
+
+    return sketch.face()
--- cad/tests/key_locations.py
+++ cad/tests/key_locations.py
@@ -1,20 +1,33 @@
 import typing
 import yaml
+from math import cos, sin, pi
 
 import build123d as bd
 from ocp_vscode import show
 
-from androphage_common import key_locations, Align
+from androphage_common import *
 
 with open('/home/joshua/Repositories/androphage/cad/androphage.yaml') as yamlfile:
     yamldict = yaml.safe_load(yamlfile)
-kl = key_locations(yamldict['columns'], yamldict['spacing'])
-with bd.BuildPart() as keys:
+spacing = bd.Vector(yamldict['spacing'])
+kl = key_locations(yamldict['columns'], spacing)
+trackball_position = 55
+with bd.BuildPart(bd.Plane.XY) as keys:
+    with bd.Locations((
+        trackball_position*cosd(45),
+        spacing.Y + trackball_position*sind(45)
+    )):
+        bd.Cylinder(radius=17, height=4)
     with kl.locations():
         bd.Box(
-            yamldict['spacing'][0],
-            yamldict['spacing'][1],
+            spacing.X,
+            spacing.Y,
             1,
-            align=Align.LeftFront
+            align=Align.LeftFrontBottom
         )
-show(keys)
+keys.label = 'Keys'
+with bd.BuildPart(bd.Plane.XY) as plate:
+    bd.extrude(plate_outline(kl, spacing, 80), -1)
+plate.color = 'Thistle'
+plate.label = 'Plate outline'
+show(keys, plate)
