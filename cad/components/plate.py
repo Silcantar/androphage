cf3bf7a1a1b4e3327f7cb751476450449349047c
Joshua Lucas <joshdlucas@gmail.com>
2026-03-14 00:18:22 -0500

    Start writing code to generate the plates.

--- cad/components/plate.py
+++ cad/components/plate.py
@@ -1,18 +1,36 @@
 import typing
+from enum import StrEnum, auto
 
 import build123d as bd
 
-from androphage_common import *
+from common import *
+
+class PlateType(StrEnum):
+    BOTTOM = auto()
+    PCB = auto()
+    SWITCH = auto()
+    TOP = auto()
 
 class Plate(Component):
     def __init__(
         self,
-        align: AlignLike = bd.Align.NONE,
-        color: bd.ColorLike | str = 'silver',
-        mode: bd.Mode = bd.Mode.ADD,
-        rotation: bd.RotationLike = [0, 0, 0],
+        key_locations: LocationDict = None,
+        spacing: bd.VectorLike = (19, 19),
+        thickness: float = 1.6,
+        plate_type: PlateType = PlateType.SWITCH,
+        **kwargs
     ):
-        super().__init__(align, color, mode, rotation)
+        self.key_locations = key_locations
+        self.thickness = thickness
+        self.plate_type = plate_type
+        super().__init__(**kwargs)
 
     def build(self):
-        pass
+        outline = plate_outline(self.key_locations)
+
+if __name__ == '__main__':
+    from ocp_vscode import show
+    from parameters import Parameters
+    params = Parameters.from_yaml_file('../androphage.yaml')
+    kl = key_locations(params)
+    show(Plate(kl, params['spacing'], params['plate_thickness']))
