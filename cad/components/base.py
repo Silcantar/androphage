c7e471d3044c3e6c7636b4f13b3f242308277c5e
Joshua Lucas <joshdlucas@gmail.com>
2026-06-05 21:58:46 -0500

    Re-integrate eye code into base module.

--- cad/components/base.py
+++ cad/components/base.py
@@ -164,13 +164,67 @@ class Base(Component):
         base.label = "Base"
         base.color = self.color
         # Add eye.
-        from components.eye import Eye
-        eye = eye_location * Eye(self.parameters)
-        return bd.Part(children=[base, eye])
+        (iris, pupil) = (eye_location * part for part in self._eye())
+        return bd.Part(children=[base, iris, pupil])
+
+    def _eye(self) -> tuple[bd.Part]:
+        p = self.parameters
+        iris_size = bd.Vector(p.Eye.size)
+        iris_arc = bd.Plane.YZ * bd.SagittaArc(
+            start_point=(-iris_size.X/2, 0, 0),
+            end_point=(iris_size.X/2, 0, 0),
+            sagitta=iris_size.Y
+        )
+        iris_sketch = bd.make_face(bd.Wire(iris_arc).close())
+        iris_sketch = bd.split(
+            objects=iris_sketch,
+            bisect_by=bd.Plane.XZ,
+            keep=bd.Keep.TOP
+        )
+        iris = bd.revolve(
+            profiles=iris_sketch,
+            axis=bd.Axis.Z,
+            revolution_arc=180
+        )
+        iris.label = "Iris"
+        iris.color = seq_to_color(p.Eye.color)
+        pupil_size = bd.Vector(p.Eye.pupil_size)
+        pupil_arc = bd.SagittaArc(
+            start_point=(0, -pupil_size.X/2, 0),
+            end_point=(0, pupil_size.X/2, 0),
+            sagitta=-pupil_size.Y/2
+        )
+        pupil_sketch = bd.make_face(bd.Wire(pupil_arc).close())
+        pupil = bd.extrude(
+            to_extrude=pupil_sketch,
+            target=iris,
+            until=bd.Until.LAST
+        )
+        pupil.label = "Pupil"
+        pupil.color = "Black"
+        iris -= pupil
+        # The draft operation failed so accomplish the same thing with a cut.
+        draft_cutter = bd.Box(
+            length=BIG,
+            width=BIG,
+            height=BIG,
+            rotation=(0, p.tent_angle, 0),
+            align=Align.Right
+        )
+        iris -= draft_cutter
+        pupil -= draft_cutter
+        return (iris, pupil)
+
+    def _pupil(self) -> bd.Part:
+        p = self.parameters
+        return pupil
 
     def _sweep_paths(self) -> list[bd.Edge]:
         p = self.parameters
-        hinge_boss_thickness = (p.Hinge.thickness + p.Insert.hole_depth)/cosd(p.tent_angle)
+        hinge_boss_thickness = (
+            p.Hinge.thickness 
+            + p.Insert.hole_depth
+            )/cosd(p.tent_angle)
         return [
             bd.Line(
                 (hinge_boss_thickness, 0, 0),
@@ -225,4 +279,4 @@ if __name__ == "__main__":
     p = layout.set_derived_parameters(
         parameters.load_parameters("cad/androphage.yaml")
     )
-    show(Base(p))
+    show(Base(p, mirror=True))
